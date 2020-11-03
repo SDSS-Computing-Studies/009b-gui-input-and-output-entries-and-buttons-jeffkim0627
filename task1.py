2da98823525a621d49e4154aa7c6daccd72bc1f2
jeffkim0627 <55156837+jeffkim0627@users.noreply.github.com>
2020-11-03 13:06:24 -0800

    update

--- task1.py
+++ task1.py
@@ -6,3 +6,31 @@ the completed madlib.
 What is a madlib? Visit https://www.madlibs.com/printables/ to see some Madlibs
 you might use in your assignment
 """
+
+#Part invitation letter
+
+import tkinter as tk 
+from tkinter import *
+
+win = tk.Tk()
+
+
+label1 = Label(win, width=15, text="Dear " textvariable=labelData)
+e1 = Entry(win, text="Noun, ", width=10)
+label2 = Label(win, width=15, text="you " textvariable=labelData)
+e2 = Entry(win, text="are/aren't" width=10)
+label
+
+
+
+e1.pack()
+e2.pack()
+
+
+
+
+
+
+
+
+win.mainloop()
